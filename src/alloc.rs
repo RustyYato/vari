41a764cbcae4d8ca88ab0870b9c43053d6f0a328
RustyYato <krishna.sd.2012@gmail.com>
2020-11-14 19:57:11 -0500

    simplify AllocStrategy implementation

--- src/alloc.rs
+++ src/alloc.rs
@@ -3,16 +3,25 @@ use core::alloc::Layout;
 use crate::internals::TypeList;
 
 pub unsafe trait AllocStrategy<L: TypeList>: Clone {
+    #[inline]
     fn layout(&self, index: usize) -> Layout {
         assert!(L::COUNT > index);
 
         unsafe { self.layout_unchecked(index) }
     }
 
-    unsafe fn layout_unchecked(&self, index: usize) -> Layout;
+    #[inline]
+    unsafe fn matches_type_layout<T>(&self, current: usize) -> bool {
+        self.matches_layout(current, crate::internals::layout::<T>(L::ALIGN))
+    }
 
-    unsafe fn matches_type_layout<T>(&self, current: usize) -> bool;
-    unsafe fn matches_index_layout(&self, current: usize, other: usize) -> bool;
+    #[inline]
+    unsafe fn matches_index_layout(&self, current: usize, other: usize) -> bool {
+        self.matches_layout(current, self.layout_unchecked(other))
+    }
+
+    unsafe fn layout_unchecked(&self, index: usize) -> Layout;
+    unsafe fn matches_layout(&self, current: usize, layout: Layout) -> bool;
 }
 
 pub type DefaultStrategy = BiggestVariant;
@@ -25,31 +34,25 @@ pub struct BiggestVariant;
 pub struct Minimal;
 
 unsafe impl<L: TypeList> AllocStrategy<L> for BiggestVariant {
+    #[inline]
     unsafe fn layout_unchecked(&self, _: usize) -> Layout {
         L::layout_max_unchecked(Layout::from_size_align_unchecked(0, L::ALIGN))
     }
 
-    unsafe fn matches_type_layout<T>(&self, _: usize) -> bool {
-        true
-    }
-
-    unsafe fn matches_index_layout(&self, _: usize, _: usize) -> bool {
+    #[inline]
+    unsafe fn matches_layout(&self, _: usize, _: Layout) -> bool {
         true
     }
 }
 
 unsafe impl<L: TypeList> AllocStrategy<L> for Minimal {
+    #[inline]
     unsafe fn layout_unchecked(&self, index: usize) -> Layout {
         L::layout_min(index, L::ALIGN)
     }
 
-    unsafe fn matches_type_layout<T>(&self, current_index: usize) -> bool {
-        crate::internals::layout::<T>(L::ALIGN)
-            == AllocStrategy::<L>::layout_unchecked(self, current_index)
-    }
-
-    unsafe fn matches_index_layout(&self, current_index: usize, other_index: usize) -> bool {
-        AllocStrategy::<L>::layout_unchecked(self, current_index)
-            == AllocStrategy::<L>::layout_unchecked(self, other_index)
+    #[inline]
+    unsafe fn matches_layout(&self, current_index: usize, other: Layout) -> bool {
+        AllocStrategy::<L>::layout_unchecked(self, current_index) == other
     }
 }
